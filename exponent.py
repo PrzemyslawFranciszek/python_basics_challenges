5f063dd4c0f022b5f6295056099eba9e6db72131
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2022-12-19 21:40:49 +0100

    Challenge from excersise 5.3

---
+++ exponent.py
@@ -0,0 +1,6 @@
+# Program which rise one number to the power of second number
+
+num1 = input("Please write your first number. It will be base of exponent.\n")
+num2 = input("Write your second number. It will be exponent.\n")
+result = float(num1) ** float(num2)
+print(f"{num1} to the power of {num2}. The result is: {result}")
