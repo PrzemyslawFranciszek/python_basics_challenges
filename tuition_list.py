50d134816222cbc74157cdc0634eda30ca41aa51
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2022-12-28 18:02:03 +0100

    Enter data in lists

---
+++ tuition_list.py
@@ -0,0 +1,8 @@
+universities = [
+    ['California Institute of Technology', 2175 , 37704],
+    ['Harvard', 19627, 39849],
+    ['Massachusetts Institute of Technology' , 10566, 40732],
+    ['Princeton', 7802, 37000],
+    ['Rice', 19535, 40569],
+    ['Yale', 11701, 40500]
+]
