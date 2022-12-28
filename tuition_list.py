368087bdc3b575ca42d32303e877933d1a26f5ad
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2022-12-28 23:15:02 +0100

    add function for new list

--- tuition_list.py
+++ tuition_list.py
@@ -6,3 +6,21 @@ universities = [
     ['Rice', 19535, 40569],
     ['Yale', 11701, 40500]
 ]
+
+enrollment_values = []
+tuition_fees = []
+def enrollment_stats(item):
+    """Return 2 lists: first all student enrollment values and second all the tuition fees"""
+    
+    for item_in_list in item:
+        
+        enrollment_values.append(item_in_list[1])
+    
+        # append values to empty list 
+#    print(item[1])
+#    print([item_in_list for item_in_list in item])
+
+enrollment_stats(universities)
+print(enrollment_values)
+
+# enrollment_stats(universities)
