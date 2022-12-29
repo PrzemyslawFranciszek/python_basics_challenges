7ef5216bb1b535ecc44e61634e2ac7ece3d2f57d
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2022-12-29 16:30:31 +0100

    Program sum up total number of student. Function, lists, indices and built-up Python funkction

--- tuition_list.py
+++ tuition_list.py
@@ -3,24 +3,45 @@ universities = [
     ['Harvard', 19627, 39849],
     ['Massachusetts Institute of Technology' , 10566, 40732],
     ['Princeton', 7802, 37000],
-    ['Rice', 19535, 40569],
+    ['Rice', 5879, 35551],
+    ['Stanford', 19535, 40569],
     ['Yale', 11701, 40500]
 ]
-
-enrollment_values = []
-tuition_fees = []
-def enrollment_stats(item):
+def enrollment_stats(item_in_outer_list):
     """Return 2 lists: first all student enrollment values and second all the tuition fees"""
-    
-    for item_in_list in item:
+    enrollment_values = []
+    tuition_fees = []
+
+    for item_in_inner_list in item_in_outer_list:
         
-        enrollment_values.append(item_in_list[1])
+        # add to list value of single enrollment
+        enrollment_values.append(item_in_inner_list[1])
+        # add to list value of single tuition fee
+        tuition_fees.append(item_in_inner_list[2])
+    return enrollment_values, tuition_fees
+
+def mean(mean_arg):
+    """Returns the mean of numbers in list"""
+    return round(sum(mean_arg)/len(mean_arg), 2)
+
+def median(median_arg):
+    """Returns the median of numbers in list"""
+    median_arg.sort()
+    list_length = len(median_arg)
     
-        # append values to empty list 
-#    print(item[1])
-#    print([item_in_list for item_in_list in item])
+    if list_length % 2 == 0:
+        # if the number of values in list are even we have 2 median numbers
+        indices = (list_length // 2)   
+        return (median_arg[indices - 1] + median_arg[indices]) / 2
+    else:
+        # if the number of values in list are odd we have only one median number
+        return median_arg[(list_length // 2)]
 
-enrollment_stats(universities)
-print(enrollment_values)
+print(f"**************************\n"
+      f"Total students:   {sum((enrollment_stats(universities)[0])):,}\n"
+      f"Total tuition:  $ {sum((enrollment_stats(universities)[1])):,}\n\n"
+      f"Student mean:     {mean((enrollment_stats(universities)[0])):,}\n"
+      f"Student median:   {median(enrollment_stats(universities)[0]):,}\n\n"
+      f"Tuition mean:   $ {mean(enrollment_stats(universities)[1]):,}\n"
+      f"Tuition median: $ {median(enrollment_stats(universities)[1]):,}")
 
-# enrollment_stats(universities)
