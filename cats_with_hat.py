45864c040d24c1f42846a25029d5028639e31d0c
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2023-01-01 00:29:18 +0100

    added function

--- cats_with_hat.py
+++ cats_with_hat.py
@@ -1,6 +1,36 @@
 cats = {}
 for cat in range(1,101):
-    #add 100 cats without hats to dict
+    # add a 100 different cats to dictionary
     cats['#' + str(cat)] = ''
+counter = 0
+def every_cat():
+    """loop over every cat to add or remove values in dict keys"""
+    for cat, hat in cats.items():
+        hat = check_for_hat(hat)
+        cats[cat] = hat
 
-print(cats)
+def second_cat():
+    """loop on second cat to add or remove values in dict keys"""
+    
+##def third_cat():
+##    
+
+def check_for_hat(hat):
+    """"changes a hat variable"""
+    if hat == 'hat':
+        hat = 'not hat'
+    else:
+        hat = 'hat'
+    return hat
+
+
+while True:
+    every_cat()
+    counter  = counter +1
+    second_cat()
+    counter  = counter +1
+    third_cat()
+    counter  = counter +1
+    
+
+    
