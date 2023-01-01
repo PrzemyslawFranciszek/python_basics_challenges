656da74a0cd3812ae74638ea546f69c309ed0c8b
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2023-01-01 11:30:05 +0100

    whole program with methods and while loop

--- cats_with_hat.py
+++ cats_with_hat.py
@@ -1,8 +1,9 @@
 cats = {}
+
 for cat in range(1,101):
     # add a 100 different cats to dictionary
     cats['#' + str(cat)] = ''
-counter_round = 0
+
 def every_cat():
     """loop over every cat to add or remove values in dict keys"""
     for cat, hat in cats.items():
@@ -11,40 +12,53 @@ def every_cat():
 
 def second_cat():
     """loop on second cat to add or remove values in dict keys"""
-    counter_loop = 0
+    cats_loop = 0
     for cat, hat in cats.items():
-        counter_loop = counter_loop + 1
-        if counter_loop % 2 == 0:
+        cats_loop = cats_loop + 1
+        if cats_loop % 2 == 0:
             hat = check_for_hat(hat)
             cats[cat] = hat
+
 def third_cat():
     """loop on third cat to add or remove values in dict keys"""
-    counter_loop = 0
+    cats_loop = 0
     for cat, hat in cats.items():
-        counter_loop = counter_loop + 1
-        if counter_loop % 3 == 0:
+        cats_loop = cats_loop + 1
+        if cats_loop % 3 == 0:
             hat = check_for_hat(hat)
             cats[cat] = hat
-    print(cats)
-##def last_round():
+
+def last_round():
+    """changes value of the last key"""
+    if cats['#100'] ==  'hat':
+        cats['#100'] = ''
+    else:
+        cats['#100'] = 'hat'
 
 def check_for_hat(hat):
     """"changes a hat variable"""
     if hat == 'hat':
-        hat = 'not hat'
+        hat = ''
     else:
         hat = 'hat'
     return hat
+def print_cat():
+    """print cats which have hats"""
+    for cat, hat in cats.items():
+        if hat == 'hat':
+            print(cat)
 
-
+counter_round = 0
 while True:
-    every_cat()
-    counter_round  = counter_round +1
-    second_cat()
-    counter_round  = counter_round +1
-    third_cat()
-    counter_round  = counter_round +1
-    break
-    
+    if counter_round < 100:
+        every_cat()
+        counter_round  = counter_round + 1
+        second_cat()
+        counter_round  = counter_round + 1
+        third_cat()
+        counter_round  = counter_round + 1
+    else:
+        last_round()
+        print_cat()
+        break
 
-    
