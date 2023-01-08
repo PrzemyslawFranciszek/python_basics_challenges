28c5d908f4bb24233a8a8e631e11eb986ca37be1
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2023-01-08 11:35:19 +0100

    aded function walk to main class animal

--- model_a_farm.py
+++ model_a_farm.py
@@ -1,12 +1,30 @@
 class Animal:
+    exhaustion = 0
     def __init__(self, name, age):
         self.name = name
         self.age = age
     def speak(self, sound):
         return f"The {self.name} says {sound}"
+    def walk(self, distance):
+        try:
+            if distance >= 0:
+                if self.exhaustion + distance < 11:
+                    self.exhaustion = self.exhaustion + distance
+            else:
+                return "You must enter value in positive number!"                    
+        except TypeError:
+            return "You must enter distance in numbers!"
 
 class Cow(Animal): 
     def speak(self, sound = "MUU"):
         return super().speak(sound)
-    
+
+class Duck(Animal):
+    def speak(self, sound = "KWAA"):
+        return super().speak(sound)
+
+class Pig(Animal):
+    def speak(self,sound = "OINKK"):
+        return super().speak(sound)
 bessie = Cow("Bessie", 5)
+babe = Pig("Babe", 1)
