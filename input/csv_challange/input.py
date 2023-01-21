1e8c373b8c0b73eed8713154b8b588c537e4cff5
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2023-01-21 12:41:21 +0100

    input csv

--- input/csv_challange/input.py
+++ input/csv_challange/input.py
@@ -1,7 +1,13 @@
-import pathlib 
+import pathlib , csv
 
-path_file = pathlib.Path.cwd() / "input" / "csv_challange"
-path_file.mkdir(parents = True, exist_ok = True)
-source = path_file / ".." / ".." / "input.py"
-destination = path_file / "input.py"
-source.replace(destination)
+path_file = pathlib.Path.cwd()
+scores_file = path_file / "scores.csv"
+scores = [{"name": "LLCoolDave", "score":"23"},
+          {"name": "LLCoolDave", "score":"27"},
+          {"name": "red", "score":"12"},
+          {"name": "LLCoolDave", "score":"26"},
+          {"name": "tom123", "score":"26"}]
+with scores_file.open(mode="w", encoding="utf-8", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames = ["name", "score"])
+    writer.writeheader()
+    writer.writerows(scores)
